5cdc3bfc9e8b7b4c0382ade085940fad2ecbb6cb
DESKTOP-0CDRRID\Administrador <suporte@univali.br>
2025-09-29 14:14:46 -0300

    first commit

--- index.js
+++ index.js
@@ -1,64 +1,64 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-
 
 const PORT = 3000;
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const publicDir = path.join(__dirname, './');
+const publicDir = path.join(__dirname, "./public");
 
 let pessoas = [
-    {
-        id: 1,
-        nome: 'Lara',
-        login: 'admin',
-        senha: '123',
-        idade: 14,
-        irmaos: true,
-        cidade: 'São Paulo',
-        hobby: 'Desenhar'
-    },
-    {
-        id: 2,
-        nome: 'Gaby',
-        login: 'admin1',
-        senha: '123',
-        idade: 13,
-        irmaos: false,
-        cidade: 'Rio de Janeiro',
-        hobby: 'Tocar violão'
-    },
-    {
-        id: 3,
-        nome: 'Anna Laura',
-        login: 'admin3',
-        senha: '123',
-        idade: 14,
-        irmaos: true,
-        cidade: 'Belo Horizonte',
-        hobby: 'Dançar'
-    },
-    {
-        id: 4,
-        nome: 'Yasmin',
-        login: 'admin4',
-        senha: '123',
-        idade: 13,
-        irmaos: true,
-        cidade: 'Salvador',
-        hobby: 'Ler livros'
-    },
-    {
-        id: 5,
-        nome: 'Lynne',
-        login: 'admin5',
-        senha: '123',
-        idade: 13,
-        irmaos: true,
-        cidade: 'Curitiba',
-        hobby: 'Jogar videogame'
-    },
+  {
+    id: 1,
+    nome: "Lara",
+    login: "admin",
+    senha: "123",
+    idade: 14,
+    irmaos: true,
+    cidade: "São Paulo",
+    hobby: "Desenhar",
+  },
+  {
+    id: 2,
+    nome: "Gaby",
+    login: "admin1",
+    senha: "123",
+    idade: 13,
+    irmaos: false,
+    cidade: "Rio de Janeiro",
+    hobby: "Tocar violão",
+  },
+  {
+    id: 3,
+    nome: "Anna Laura",
+    login: "admin3",
+    senha: "123",
+    idade: 14,
+    irmaos: true,
+    cidade: "Belo Horizonte",
+    hobby: "Dançar",
+  },
+  {
+    id: 4,
+    nome: "Yasmin",
+    login: "admin4",
+    senha: "123",
+    idade: 13,
+    irmaos: true,
+    cidade: "Salvador",
+    hobby: "Ler livros",
+  },
+  {
+    id: 5,
+    nome: "Lynne",
+    login: "admin5",
+    senha: "123",
+    idade: 13,
+    irmaos: true,
+    cidade: "Curitiba",
+    hobby: "Jogar videogame",
+  },
 ];
 
 // ========================================
@@ -70,36 +70,33 @@ let pessoas = [
 // Endpoint: http://localhost:3000/
 // Função: Verificar se a API está funcionando
 app.get("/", (req, res) => {
-    res.sendFile(path.join(publicDir, "itens.html"));
+  res.sendFile(path.join(publicDir, "login.html"));
 });
 
-app.post('/login', (req, res) => {
-    const { login, senha } = req.body
+app.post("/login", (req, res) => {
+  const { nome, login, senha } = req.body;
 
-    //verifica se um dos campos vieram vazios
-    if (!login || !senha) {
-        res.status(404).json({
-            status: 404,
-            message: "Requisição inválida"
-        })
-    }
+  if (!nome || !login || !senha) {
+    return res
+      .status(400)
+      .json({ message: "Nome, login e senha são obrigatórios" });
+  }
 
-    const usuario = pessoas.find((p) => p.login === login)
-    if (!usuario) {
-        res.status(404).json({
-            status: 404,
-            message: "Usuário não encontrado"
-        })
-    }
-    if (usuario.senha !== senha) {
-        res.status(404).json({
-            status: 404,
-            message: "Senha inválida"
-        })
-    }
-    res.status(200).json({ status: 200, message: "Login com sucesso" })
-})
+  const pessoaExiste = pessoas.findIndex((p) => p.login === login);
+  if (pessoaExiste) {
+    res.status(404).json("pessoa já existe");
+  }
+
+  const novaPessoa = {
+    id: pessoas.length + 1,
+    nome: req?.body.nome,
+    login: req?.body.login,
+    senha: req?.body.senha,
+  };
+  pessoas.push(novaPessoa);
+  res.status(201).json("pssoa criada com sucesso");
+});
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
